dfef8e0b2255bfdbf2d4c604a0aba1533c45065a
Christian-Fisher <christian.fisher@carleton.ca>
2020-02-01 18:06:41 -0500

    Added comments to FloorSubSystem

--- src/FloorSubsystem.java
+++ src/FloorSubsystem.java
@@ -6,40 +6,58 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
+
+/**
+ * @author Christian Fisher
+ * @version 1.0
+ * The Floor Subsystem is responsible for reading the requests from a textfile and sending these requests to the scheduler.
+ * 
+ * 
+ */
 public class FloorSubsystem implements Runnable {
 
-	private Scheduler scheduler;
-	ArrayList<RequestData> dataArray = new ArrayList<RequestData>();
+	private Scheduler scheduler;	//Contains the reference to the scheduler
+	ArrayList<RequestData> dataArray = new ArrayList<RequestData>();	//Holds all the data read from the file
 	public FloorSubsystem(Scheduler scheduler) {
 
 		this.scheduler = scheduler;
 		readDataFromFile();
 	}
 
+	/**
 	@Override
+	This method will be run when this thread is started. Run will iterate through the dataArray and send 
+	every item to the scheduler.
+	*/
 	public void run() {
-		Iterator<RequestData> dataArrayIterator = dataArray.iterator();
-		while(dataArrayIterator.hasNext()) {
-			scheduler.placeRequest(dataArrayIterator.next());			
+		Iterator<RequestData> dataArrayIterator = dataArray.iterator(); 	//Creates a iterator
+		while(dataArrayIterator.hasNext()) {								//Iterates through the arraylist
+			scheduler.placeRequest(dataArrayIterator.next());				//Sends the request to the scheduler
 			
 		}
 		
 	}
 
+	
+	/**
+	 * readDataFromFile takes all requests from the inputFile.txt. The method will then take the read 
+	 * string and convert it into a RequestData stucture.
+	 * 
+	 */
 	private void readDataFromFile() {
-		File inputFile = new File("inputFile.txt");
+		File inputFile = new File("inputFile.txt");	//Creates the file. For testing the file is inputFile.txt
 		try {
-			Scanner fileReader = new Scanner(inputFile);
-			while(fileReader.hasNextLine()) {
-				String line = fileReader.nextLine();
-				String[] splitLine = line.split(" ");
-				if(splitLine[2].equals("Up")) {
+			Scanner fileReader = new Scanner(inputFile);	//Creates a scanner to read the file
+			while(fileReader.hasNextLine()) {	//Iterates through the file
+				String line = fileReader.nextLine();	
+				String[] splitLine = line.split(" ");	//Splits each catagory into an array of strings
+				if(splitLine[2].equals("Up")) {	//Checks the direction in the request, and adds the RequestData object to the arrayList based on the Direction
 					dataArray.add(new RequestData(new SimpleDateFormat("HH:mm:ss.SSS").parse(splitLine[0]), Integer.parseInt(splitLine[1]), Direction.UP, Integer.parseInt(splitLine[3])));
 				}else if(splitLine[2].equals("Down")) {
 					dataArray.add(new RequestData(new SimpleDateFormat("HH:mm:ss.SSS").parse(splitLine[0]), Integer.parseInt(splitLine[1]), Direction.DOWN, Integer.parseInt(splitLine[3])));
 				}
 			}
-			fileReader.close();
+			fileReader.close();//Closes the file
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (NumberFormatException e) {
